661676d3ba47275a437fadbcee1a6800d155ff90
salsadsid <salman.dnj@gmail.com>
2022-04-16 04:30:57 +0600

    Implement-private-route-checkout-component

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Register.css'
+import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate()
     const navigateToLogin = () => {
         navigate('/login')
+    }
+
+    if (user) {
+        navigate('/home');
     }
     const handleRegister = event => {
         event.preventDefault()
@@ -12,7 +25,7 @@ const Register = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
 
-        console.log(name, email, password);
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='register-form'>
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {useParams} from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 const ServiceDetail = () => {
-    const {serviceId}=useParams()
+    const { serviceId } = useParams()
     return (
         <div>
             <h2>This is Service Detail:{serviceId}</h2>
+            <div className='text-center'>
+                <Link to='/checkout'>
+                    <button className='btn btn-primary'>Proceed CheckOut</button>
+                </Link>
+            </div>
         </div>
     );
 };
